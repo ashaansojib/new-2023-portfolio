5a77ab94e8e9ecd6b9ca6ab6b4c0bcb8f08a876b
ashaansojib <ashaduzzamansojib67@gmail.com>
2023-08-14 02:55:34 +0600

    add services

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../shared/Header';
+import Service from '../pages/services/Service';
 
 const Home = () => {
     return (
         <div>
             <Header />
+            <Service />
         </div>
     );
 };
---
+++ src/pages/services/Service.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Service = () => {
+    return (
+        <section className='my-container bg-[#022642] px-4 py-20'>
+            {/* section title */}
+            <div className='text-center pb-8'>
+                <p>MY EXPERIENCE</p>
+                <h3 className='text-4xl font-semibold'>What I Do</h3>
+            </div>
+            {/* content */}
+            <div className='grid grid-cols-3 gap-4'>
+                <div>
+                    <img className='w-[100px] h-[120px] py-3' src="/1-1.png" alt="" />
+                    <h4 className='text-2xl font-bold py-2'>Front-End Development</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste id doloribus dolorum consequuntur cupiditate eligendi vel? Natus quibusdam, illo architecto harum quis similique, asperiores eaque perspiciatis dolorum, repellendus ipsum facilis.</p>
+                </div>
+                <div>
+                    <img className='w-[100px] h-[120px] py-3' src="/2.png" alt="" />
+                    <h4 className='text-2xl font-bold py-2'>Jr. Backend Developer</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste id doloribus dolorum consequuntur cupiditate eligendi vel? Natus quibusdam, illo architecto harum quis similique, asperiores eaque perspiciatis dolorum, repellendus ipsum facilis.</p>
+                </div>
+                <div>
+                    <img className='w-[100px] h-[120px] py-3' src="/4.png" alt="" />
+                    <h4 className='text-2xl font-bold py-2'>MERN Stack Developer</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste id doloribus dolorum consequuntur cupiditate eligendi vel? Natus quibusdam, illo architecto harum quis similique, asperiores eaque perspiciatis dolorum, repellendus ipsum facilis.</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Service;
